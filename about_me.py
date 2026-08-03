23d5d71bf8ee08d4316ac69a95b72b60a3c1608f
MysteryJosh <joshuaakosa@gmail.com>
2026-08-03 11:36:53 -0500

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,32 @@
+# About Me Variables
+first_name = "Joshua"
+last_name = "Akosa"
+age = 27
+city = "Fort Worth"
+previous_job = "Gamestop ROC"
+why_coding = "I have always had interest in coding"
+fun_fact = "I enjoy watching all the major sports"
+
+
+
+print("=" * 40)
+print("Student Profile")
+print("=" * 40)
+
+# About Me Input
+print("\nName:", first_name, last_name)
+print("Age:", age)
+print("City:", city)
+print("Previous Job:", previous_job)
+
+print("Why Coding:", why_coding)
+
+print("Fun Fact:", fun_fact)
+print("=" * 40)
+
+# This is a baisc info about myself.
+# All About me is for a lesson practice.
+
+print(f"\n{first_name} {last_name} is {age} and lives in {city}.")
+
+# I used the f string just to have it formatted
